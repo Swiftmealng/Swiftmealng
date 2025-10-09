db4927674ed2ade55a4990c1dcebc90b85feac01
damola <damolaadegbite0@gmail.com>
2025-10-09 08:11:48 +0100

    feat: update AppError utility

---
+++ server/src/utils/AppError.ts
@@ -0,0 +1,40 @@
+class AppError extends Error {
+  statusCode: number;
+  status: string;
+  isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string = 'Resource not found') {
+    super(message, 404);
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message: string = 'Validation failed') {
+    super(message, 400);
+  }
+}
+
+export class AuthenticationError extends AppError {
+  constructor(message: string = 'Authentication failed') {
+    super(message, 401);
+  }
+}
+
+export class AuthorizationError extends AppError {
+  constructor(message: string = 'You do not have permission to perform this action') {
+    super(message, 403);
+  }
+}
+
+export default AppError;
